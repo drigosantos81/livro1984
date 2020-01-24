868ca1cc0290c28605dd1dfd17c93698d3f06fd4
Rodrigo Emanuel <drigosantos@gmail.com>
2020-01-24 10:31:36 -0300

    Primeiro commit

---
+++ js/init.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyDJpV16wOilgA9Hx_PuyVk3_hrop5bfJOc",
+    authDomain: "livro1984-f518e.firebaseapp.com",
+    databaseURL: "https://livro1984-f518e.firebaseio.com",
+    projectId: "livro1984-f518e",
+    storageBucket: "livro1984-f518e.appspot.com",
+    messagingSenderId: "185702894590",
+    appId: "1:185702894590:web:d1bf88ce3593a026610eaf"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
